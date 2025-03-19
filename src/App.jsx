401bfa30711f7f51e57dbce9bc23ee62bf264daa
Student-shivamsharma <sgivamsharma06@gmail.com>
2025-03-20 01:46:21 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,6 @@
 
 
 
-// import React, { useState, useEffect } from "react";
-// import "./App.css";
-// import Splash from "./components/Splash";
-
-// import { Events } from "./components/Events";
-// import Home from "./components/Home";
-// import { TeamCards } from "./components/TeamCards";
-// import { BrowserRouter as Router, Route, Routes , useLocation} from "react-router-dom";
-// // import Contact from "./components/Contact";
-// import { Navbar } from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import { Register } from "./components/Register";
-// import { Edevclash } from "./components/Edevclash";
-// import {Egraphicon} from "./components/Egraphicon";
-// import {Ehackeroverflow} from "./components/Ehackeroverflow";
-// import {Enimbus} from "./components/Enimbus";
-// import {Espocc} from "./components/Espocc";
-// import { Contact } from "./components/Contact";
-
- 
-// const App = () => {
-//   const [isLoaded, setIsLoaded] = useState(false);
-  
-  
-//   const location = useLocation();
- 
-//   useEffect(() => {
-//     const timer = setTimeout(() => {
-//       setIsLoaded(true);
-//     }, 4000); // 4 seconds
-
-   
-//     return () => clearTimeout(timer);
-//   }, []);
-
-  
-//     useEffect(() => {
-//       if(location.pathname === "Register"){
-//         document.title = "404 not found";
-  
-//       }
-//       else {
-//         document.title = "Cloud Computing Cell - AKGEC";
-//       }
-//     },[location])
-
-//   return (
-//     <Router>
-     
-//       {isLoaded ? (
-//         <>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/Events" element={<Events />} />
-//           <Route path="/Team" element={<TeamCards />} />
-//           <Route path="/Contact" element={<Contact />} />
-//           <Route path="/Register" element={<Register />} />
-//           <Route path="/Devclash" element={<Edevclash />} />
-//           <Route path="/Graphicon" element={<Egraphicon />} />
-//           <Route path="/HackerOverflow" element={<Ehackeroverflow />} />
-//           <Route path="/Nimbus" element={<Enimbus />} />
-//           <Route path="/Spocc" element={<Espocc />} />
-//         </Routes>
-//           <Navbar />
-//            <Footer />
-//           </>
-//       ) : (
-       
-//         <Splash />
-//        )} 
-//     // </Router>
-//   );
-// };
-
-// export default App;
-
-
-
-
-
 
 
 import React, { useState, useEffect } from "react";
@@ -94,8 +14,6 @@ import { Navbar } from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Register } from "./components/Register";
 import { Edevclash } from "./components/Edevclash";
-// import { Egraphicon } from "./components/Ereacticons";
-// import { Ehackeroverflow } from "./components/Ehackeroverflow";
 import { Enimbus } from "./components/Enimbus";
 import { Techbuzz } from "./components/ETechbuzz";
 import { Contact } from "./components/Contact";
